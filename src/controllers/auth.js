a84590ad337be983e18cc6c16cd817ba61c8297b
kurnia septia wiguna <kurniaseptiawiguna9@gmail.com>
2021-11-24 09:32:59 +0700

    register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,7 +2,6 @@ const {user} = require('../../models');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
 exports.login = async (req, res) => {
   // our validation schema here
   const schema = Joi.object({
@@ -60,4 +59,70 @@ exports.login = async (req, res) => {
       message: "Server Error",
     });
   }
+};
+
+exports.register = async (req, res) => {
+  const schema = Joi.object({
+      email: Joi.string().email().min(6).required(),
+      password: Joi.string().min(6).required(),
+      fullname: Joi.string().min(5).required(),
+      gender: Joi.string().required(),
+      phone: Joi.string().min(11).required(),
+      address: Joi.string().required(),
+      
+  })
+
+  const {error} = schema.validate(req.body);
+
+  if (error)
+  return res.status(400).send({
+    error: {
+      message: error.details[0].message,
+    },
+  });
+
+try {
+  const userExist = await user.findOne({
+      where: {
+        email: req.body.email,
+      },
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+
+    if(userExist){
+        return res.send({
+            status: "failed",
+            message: "already exist"
+        })
+    }
+  const salt = await bcrypt.genSalt(10);
+  // we hash password from request with salt
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  const newUser = await user.create({
+      email: req.body.email,
+      password: hashedPassword,
+      fullname: req.body.fullname,
+      gender: req.body.gender,
+      phone: req.body.phone,
+      address: req.body.address,
+      photo: "default-profile.png",
+      status: "user"
+  });
+  
+  const token = jwt.sign({ id: user.id,status: user.status }, process.env.TOKEN_KEY);
+
+  res.send({
+    msg: "success",
+    token: token 
+  });
+} catch (error) {
+  console.log(error);
+  res.send({
+    status: "failed",
+    message: "Server Error",
+  });
+}
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const {home} = require('../controllers/dummy');
-const {login} = require('../controllers/auth');
+const {login,register} = require('../controllers/auth');
 const {getUsers} = require('../controllers/user')
 const router = express.Router();
 
 router.get("/", home);
 router.post("/login",login);
+router.post("/register", register);
 router.get("/users",getUsers)
 module.exports = router;
